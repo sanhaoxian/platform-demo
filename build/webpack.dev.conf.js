b039d8e959a9ef6ead85b2fc4ef2571f37474906
sanhaoxian@icloud.com <sanhaoxian@icloud.com>
2019-04-22 23:44:23 +0800

    尝试修改热更新

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,26 +1,32 @@
 const path = require('path');
 const webpack = require("webpack");
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 const merge = require("webpack-merge");
 const webpackConfigBase = require('./webpack.base.conf');
 const webpackConfigDev = {
 	mode: 'development', // 通过 mode 声明开发环境
 	output: {
+		publicPath:  '/',//必须加publicPath
 		path: path.resolve(__dirname, '../dist'),
 		// 打包多出口文件
 		// 生成 a.bundle.[hash].js  b.bundle.[hash].js
 		filename: './js/[name].bundle.js'
 	},
+	devtool: 'inline-source-map',
 	devServer: {
 		contentBase: path.join(__dirname, "../src"),
 		publicPath:'/',
 		host: "127.0.0.1",
-		port: "8089",
-		overlay: true, // 浏览器页面上显示错误
+		port: "8080",
+		hot: true, // 开启热更新
+		hotOnly:true,
+		overlay: true // 浏览器页面上显示错误
 		// open: true, // 开启浏览器
 		// stats: "errors-only", //stats: "errors-only"表示只打印错误：
-		hot: true // 开启热更新
+		
 	},
 	plugins: [
+		// new CleanWebpackPlugin(['dist']),
 		//热更新
 		new webpack.HotModuleReplacementPlugin(),
 	],
--- src/pages/three/index.js
+++ src/pages/three/index.js
@@ -1,26 +1,59 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-var animate = function () {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-};
-
-animate();
+// once everything is loaded, we run our Three.js stuff.
+function init() {
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    var scene = new THREE.Scene();
+    // create a camera, which defines where we're looking at.
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // create a render and set the size
+    var renderer = new THREE.WebGLRenderer();
+    // renderer.setClearColorHex();
+    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // show axes in the screen
+    var axes = new THREE.AxisHelper(20);
+    scene.add(axes);
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(60, 20);
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // rotate and position the plane
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    // add the plane to the scene
+    scene.add(plane);
+    // create a cube
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    // position the cube
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+    // add the cube to the scene
+    scene.add(cube);
+    // create a sphere
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    // position the sphere
+    sphere.position.x = 20;
+    sphere.position.y = 4;
+    sphere.position.z = 2;
+    // add the sphere to the scene
+    scene.add(sphere);
+    // position and point the camera to the center of the scene
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+    // add the output of the renderer to the html element
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+    // render the scene
+    renderer.render(scene, camera);
+}
+window.onload = init;
